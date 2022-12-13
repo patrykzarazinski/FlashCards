368744a70cc79045ce9782ab6a4fcbd90c2995e2
Patryk Zaraziński <patrykzarazinski1a@gmail.com>
2022-12-13 18:36:03 +0100

    Introduction database 1) Added database file 2) Added Database class for future usage

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,4 +1,25 @@
 from flask import Flask, render_template
+import sqlite3
+
+DATABASENAME = "words.db"
+
+
+class Database:
+    def __init__(self):
+        try:
+            self._connection = sqlite3.connect(DATABASENAME)
+            self._cursor = self._connection.cursor()
+        except Exception as e:
+            print(f"Can not connect to database. Exception occurred: {e}")
+
+    def execute(self, sql, parameters=(), /):
+        self._cursor.execute(sql, parameters)
+        self._connection.commit()
+
+    def __del__(self):
+        self._cursor.close()
+        self._connection.close()
+
 
 app = Flask(__name__)
 
@@ -14,4 +35,5 @@ def action():
 
 
 if __name__ == "__main__":
+    db = Database()
     app.run(host="0.0.0.0", port=5000, debug=False)
