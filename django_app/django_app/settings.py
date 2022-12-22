7e5d06f954a6e089bf8660842f2f561a2c2d4ede
Patryk Zaraziński <patrykzarazinski1a@gmail.com>
2022-12-22 23:29:39 +0100

    Security refactor 1) python-decouple lib was added to protect SECRET_KEY 2) SECRET_KEY was changed 3) .gitignore was updated 4) requirements.txt was updated 5) database was deleted. It is now hidden by .gitignore

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i1s)_5l0+&kv3sd1cw&if%(8@nww&!q@*7!3$!lm*1$*3@$v(4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
